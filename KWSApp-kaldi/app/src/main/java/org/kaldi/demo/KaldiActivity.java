f9936641841f41d5ec0786152ae2aee894895d40
eddie.lin <R07922104@ntu.edu.tw>
2020-08-06 15:40:02 +0800

    vosk-api

---
+++ KWSApp-kaldi/app/src/main/java/org/kaldi/demo/KaldiActivity.java
@@ -0,0 +1,288 @@
+// Copyright 2019 Alpha Cephei Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.kaldi.demo;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.kaldi.Assets;
+import org.kaldi.KaldiRecognizer;
+import org.kaldi.Model;
+import org.kaldi.RecognitionListener;
+import org.kaldi.SpeechRecognizer;
+import org.kaldi.Vosk;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.ref.WeakReference;
+
+public class KaldiActivity extends Activity implements
+        RecognitionListener {
+
+    static {
+        System.loadLibrary("kaldi_jni");
+    }
+
+    static private final int STATE_START = 0;
+    static private final int STATE_READY = 1;
+    static private final int STATE_DONE = 2;
+    static private final int STATE_FILE = 3;
+    static private final int STATE_MIC  = 4;
+
+    /* Used to handle permission request */
+    private static final int PERMISSIONS_REQUEST_RECORD_AUDIO = 1;
+
+
+    private Model model;
+    private SpeechRecognizer recognizer;
+    TextView resultView;
+
+    @Override
+    public void onCreate(Bundle state) {
+        super.onCreate(state);
+        setContentView(R.layout.main);
+
+        // Setup layout
+        resultView = findViewById(R.id.result_text);
+        setUiState(STATE_START);
+
+        findViewById(R.id.recognize_file).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                recognizeFile();
+            }
+        });
+
+        findViewById(R.id.recognize_mic).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                recognizeMicrophone();
+            }
+        });
+
+        // Check if user has given permission to record audio
+        int permissionCheck = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECORD_AUDIO);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, PERMISSIONS_REQUEST_RECORD_AUDIO);
+            return;
+        }
+        // Recognizer initialization is a time-consuming and it involves IO,
+        // so we execute it in async task
+        new SetupTask(this).execute();
+    }
+
+    private static class SetupTask extends AsyncTask<Void, Void, Exception> {
+        WeakReference<KaldiActivity> activityReference;
+
+        SetupTask(KaldiActivity activity) {
+            this.activityReference = new WeakReference<>(activity);
+        }
+
+        @Override
+        protected Exception doInBackground(Void... params) {
+            try {
+                Assets assets = new Assets(activityReference.get());
+                File assetDir = assets.syncAssets();
+                Log.d("KaldiDemo", "Sync files in the folder " + assetDir.toString());
+
+                Vosk.SetLogLevel(0);
+
+                activityReference.get().model = new Model(assetDir.toString() + "/model-android");
+            } catch (IOException e) {
+                return e;
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Exception result) {
+            if (result != null) {
+                activityReference.get().setErrorState(String.format(activityReference.get().getString(R.string.failed), result));
+            } else {
+                activityReference.get().setUiState(STATE_READY);
+            }
+        }
+    }
+
+    private static class RecognizeTask extends AsyncTask<Void, Void, String> {
+        WeakReference<KaldiActivity> activityReference;
+        WeakReference<TextView> resultView;
+
+        RecognizeTask(KaldiActivity activity, TextView resultView) {
+            this.activityReference = new WeakReference<>(activity);
+            this.resultView = new WeakReference<>(resultView);
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            KaldiRecognizer rec;
+            long startTime = System.currentTimeMillis();
+            StringBuilder result = new StringBuilder();
+            try {
+                rec = new KaldiRecognizer(activityReference.get().model, 16000.f);
+
+                InputStream ais = activityReference.get().getAssets().open("go.wav");
+                if (ais.skip(44) != 44) {
+                    return "";
+                }
+                byte[] b = new byte[32000];
+                int nbytes;
+                while ((nbytes = ais.read(b)) >= 0) {
+                    if (rec.AcceptWaveform(b, nbytes)) {
+                        result.append(rec.Result());
+                    } else {
+                        result.append(rec.PartialResult());
+                        continue;
+                    }
+                }
+                result.append(rec.FinalResult());
+            } catch (IOException e) {
+                return "";
+            }
+            return String.format(activityReference.get().getString(R.string.elapsed), result.toString(), (System.currentTimeMillis() - startTime));
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            activityReference.get().setUiState(STATE_READY);
+            resultView.get().append(result + "\n");
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == PERMISSIONS_REQUEST_RECORD_AUDIO) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Recognizer initialization is a time-consuming and it involves IO,
+                // so we execute it in async task
+                new SetupTask(this).execute();
+            } else {
+                finish();
+            }
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        if (recognizer != null) {
+            recognizer.cancel();
+            recognizer.shutdown();
+        }
+    }
+
+
+    @Override
+    public void onResult(String hypothesis) {
+        resultView.append(hypothesis + "\n");
+    }
+
+    @Override
+    public void onPartialResult(String hypothesis) {
+        // resultView.append(hypothesis + "\n");
+    }
+
+    @Override
+    public void onError(Exception e) {
+        setErrorState(e.getMessage());
+    }
+
+    @Override
+    public void onTimeout() {
+        recognizer.cancel();
+        recognizer = null;
+        setUiState(STATE_READY);
+    }
+
+    private void setUiState(int state) {
+        switch (state) {
+            case STATE_START:
+                resultView.setText(R.string.preparing);
+                resultView.setMovementMethod(new ScrollingMovementMethod());
+                findViewById(R.id.recognize_file).setEnabled(false);
+                findViewById(R.id.recognize_mic).setEnabled(false);
+                break;
+            case STATE_READY:
+                resultView.setText(R.string.ready);
+                ((Button) findViewById(R.id.recognize_mic)).setText(R.string.recognize_microphone);
+                findViewById(R.id.recognize_file).setEnabled(true);
+                findViewById(R.id.recognize_mic).setEnabled(true);
+                break;
+            case STATE_DONE:
+                ((Button) findViewById(R.id.recognize_mic)).setText(R.string.recognize_microphone);
+                findViewById(R.id.recognize_file).setEnabled(true);
+                findViewById(R.id.recognize_mic).setEnabled(true);
+                break;
+            case STATE_FILE:
+                resultView.setText(getString(R.string.starting));
+                findViewById(R.id.recognize_mic).setEnabled(false);
+                findViewById(R.id.recognize_file).setEnabled(false);
+                break;
+            case STATE_MIC:
+                ((Button) findViewById(R.id.recognize_mic)).setText(R.string.stop_microphone);
+                resultView.setText(getString(R.string.say_something));
+                findViewById(R.id.recognize_file).setEnabled(false);
+                findViewById(R.id.recognize_mic).setEnabled(true);
+                break;
+        }
+    }
+
+    private void setErrorState(String message) {
+        resultView.setText(message);
+        ((Button) findViewById(R.id.recognize_mic)).setText(R.string.recognize_microphone);
+        findViewById(R.id.recognize_file).setEnabled(false);
+        findViewById(R.id.recognize_mic).setEnabled(false);
+    }
+
+    public void recognizeFile() {
+        setUiState(STATE_FILE);
+        new RecognizeTask(this, resultView).execute();
+    }
+
+    public void recognizeMicrophone() {
+        if (recognizer != null) {
+            setUiState(STATE_DONE);
+            recognizer.cancel();
+            recognizer = null;
+        } else {
+            setUiState(STATE_MIC);
+            try {
+                recognizer = new SpeechRecognizer(model);
+                recognizer.addListener(this);
+                recognizer.startListening();
+            } catch (IOException e) {
+                setErrorState(e.getMessage());
+            }
+        }
+    }
+
+}
